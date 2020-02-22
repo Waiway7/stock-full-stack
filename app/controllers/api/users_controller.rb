f6e9199f1bfc5b8a610faea747ba608bd7f9bd47
ugp <wcc325@nyu.edu>
2020-02-22 15:40:50 -0500

    finished user/session controller and set up views folder

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,30 @@
+classs Api::SessionsCOntroller < ApplicationController
+    def index
+        @users = User.all
+        render "api/users/index"
+    end
+
+    def show
+        @user = User.find(params[:id])
+        render "api/users/show"
+        # else 
+        #     render ["User does not exist! Please try again."], status: 404
+        # end
+    end
+
+    def create 
+        @user = User.new(user_params)
+
+        if @user.save
+            login!(@user)
+            render "api/users/show"
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:email, :password, :username, :image)
+    end
+end
